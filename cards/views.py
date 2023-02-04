8505e87c5921e528b04d23645fc5c3dee3e9f737
Dhony Silva <dhony.web@gmail.com>
2023-02-04 16:45:42 +0000

    Implement cards functionality

--- cards/views.py
+++ cards/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect
+import random
 from django.urls import reverse_lazy
 from django.views.generic import (
     ListView,
     CreateView,
     UpdateView,
 )
+from .forms import CardCheckForm
 
 from .models import Card
 
@@ -19,4 +22,26 @@ class CardCreateView(CreateView):
 
 class CardUpdateView(CardCreateView, UpdateView):
     success_url = reverse_lazy('card-list')
+
+class BoxView(CardListView):
+    template_name = 'cards/box.html'
+    form_class = CardCheckForm
+
+    def get_queryset(self):
+        return Card.objects.filter(box=self.kwargs['box_num'])
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['box_number'] = self.kwargs['box_num']
+        if self.object_list:
+            context['card'] = random.choice(self.object_list)
+        return context
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            card = get_object_or_404(Card, id=form.cleaned_data['card_id'])
+            card.move(form.cleaned_data['solved'])
+        
+        return redirect(request.META.get('HTTP_REFERER'))
 
